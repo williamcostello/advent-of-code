bc858098f3006a7dee7783772aeda52d6c4cefa3
williamcostello <81812572+williamcostello@users.noreply.github.com>
2025-12-04 19:52:54 +0000

    2025 03: pt1

---
+++ src/2025/03-lobby/main.ts
@@ -0,0 +1,44 @@
+import { readLines } from '@/util/readLines'
+import path from 'node:path'
+
+const INPUT_FILE = path.resolve(__dirname, 'input.txt')
+
+async function main(): Promise<number> {
+    const lines = readLines(INPUT_FILE)
+
+    let maxJoltage = 0
+    for await (const line of lines) {
+        const joltages = parseLine(line)
+        maxJoltage += calculateMaxJoltage(joltages)
+    }
+
+    return maxJoltage
+}
+
+function parseLine(line: string): number[] {
+    return line.split('').map(Number)
+}
+
+function calculateMaxJoltage(joltages: number[]): number {
+    // find max thats not the last element
+    let max = 0
+    let maxIndex = -1
+    for (let i = 0; i < joltages.length - 1; i++) {
+        if (joltages[i] > max) {
+            max = joltages[i]
+            maxIndex = i
+        }
+    }
+
+    // find second max
+    let secondMax = 0
+    for (let i = maxIndex + 1; i < joltages.length; i++) {
+        if (i !== maxIndex && joltages[i] > secondMax) {
+            secondMax = joltages[i]
+        }
+    }
+
+    return max * 10 + secondMax
+}
+
+main().then(console.log)
